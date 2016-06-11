db2581decebee8ec53fc97f7a36bc476379c7f8c
Matúš Bursa <burso@burso.eu>
2016-06-11 15:32:38 +0200

    dmraid cleanup

--- config/etc/zabbix/scripts/dmraid-monitor.sh
+++ config/etc/zabbix/scripts/dmraid-monitor.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
-# dm-raid status monitor
 
-iter=0;
-raids=($(dmraid -s | awk -F':' '/name/ {gsub(/^[ \t]+/, "", $NF); print $NF}')) # array of raids
+PATH="/usr/local/zabbix-modules/fs/dmraid"
 
-display_dmraid_json() {
-        echo -e "{\n\t\"data\":["
-        while [ $iter -ne ${#raids[@]} ]; do
-                iter=$[$iter+1]
-
-                if [ $(($iter)) -ne ${#raids[@]} ];
-                    then echo -e "\t\t{ \"{#FSNAME}\":\"${raids[$(($iter-1))]}\",\t\"{#FSTYPE}\":\"dm-raid\"},"
-                else
-                    echo -e "\t\t{ \"{#FSNAME}\":\"${raids[$(($iter-1))]}\",\t\"{#FSTYPE}\":\"dm-raid\"}"; fi
-
-        done
-        echo -e "\t]\n}"
-}
-
-
-display_dmraid_status() {
-        status=$(dmraid -s | grep "$1" -A5 | awk -F':' '/status/ {gsub(/^[ \t]+/, "", $NF); print $2}')
-
-        if [ "$status" == "ok" ]; then echo 0
-        else echo 1; fi
-}
-
-if [ $# -eq 0 ]; then display_dmraid_json
-elif [ $# -eq 1 ]; then
-        dmraid=$(dmraid -s | awk -F':' "/^name   : $1$/ {print \$NF}")
-        if [ -z "$dmraid" ]; then echo "$1 doesn't exists"
-        else display_dmraid_status $dmraid; fi
-else echo -e "I don't know what do you want!"
-fi
+$PATH/dmraid.sh $1
--- modules/fs/dmraid/README.md
+++ modules/fs/dmraid/README.md
@@ -1,6 +1,2 @@
 # dmraid-zabbix
 zabbix advanced monitoring of dmraid
-
-
-* [script](https://github.com/matusso/zabbix-modules/blob/master/config/etc/zabbix/scripts/dmraid-monitor.sh) 
-* [config](https://github.com/matusso/zabbix-modules/blob/master/config/etc/zabbix/zabbix_agentd.conf)
