7414d8987b2ccb889f0b7d147866a6e70ddbbdd1
Matúš Bursa <burso@burso.eu>
2016-06-14 19:41:13 +0200

    (WIP) added new functionality

--- modules/db/mongodb/mongodb.py
+++ modules/db/mongodb/mongodb.py
@@ -4,12 +4,18 @@ import sys
 import pymongo
 import ConfigParser
 import getopt
+import json
 
 def usage():
     print "./mongodb.py -c|--config=<file> -d|--database=<dbname>"
 
 class Config:
     config_file = "conf/mongodb.conf"
+
+# mongo connection parameters
+    database = "local"
+    connection_string = "mongodb://127.0.0.1:27017"
+    replica = False
 
     def cmdline_arg(self, argv):
         try:
@@ -24,24 +30,42 @@ class Config:
                 sys.exit(0)
 
             elif opt in ("-c", "--config"):
-                config = arg
+                self.config_file = arg
 
             elif opt in ("-d", "--database"):
-                database = arg
+                self.database = arg
 
-    def config_parser(file, ):
-        print "config"
+    def config_parser(self, config_file):
+        settings = ConfigParser.ConfigParser()
+        settings.read(config_file)
+        self.connection_string = settings.get('global', 'connection_string')
+        self.replica = settings.getboolean('global', 'replica')
 
-def init_mongo():
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017") # have to define ConnectionString
-    # replicaClient = pymongo.MongoReplicaSetClient
-    print client.address
-    db = client['local'] # have to define database
-    print db.command('dbstats')
+    def __init__(self, argv):
+        self.cmdline_arg(argv)
+        self.config_parser(self.config_file)
+
+class Mongo:
+    client = None
+
+    def __init__(self, connection_string, replica):
+        if replica == True:
+            pymongo.MongoReplicaSetClient
+
+        self.client = pymongo.MongoClient(connection_string)
+
+    def get_databases(self):
+        for i in self.client.database_names():
+            print i
+
+    def get_stats(self, database):
+        db = self.client[database]
+        print db.command('dbstats')
 
 if __name__ == '__main__':
-    c = Config()
-    c.cmdline_arg(sys.argv[1:])
+    c = Config(sys.argv[1:])
+    m = Mongo(c.connection_string,
+              c.replica)
 
-
-    init_mongo()
+    m.get_databases()
+    m.get_stats("local")
