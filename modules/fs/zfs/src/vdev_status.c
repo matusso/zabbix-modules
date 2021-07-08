matusso/zabbix-modules

commit d6dab2c4034370abfdeb5b281ffce63fb164674d

new ZPOOL states + zpool_get_status now uses errata everywhere

modules/fs/zfs/src/vdev_status.c
@@ -23,12 +23,8 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
     nvlist_t * config, * nvroot;
     vdev_stat_t * vs;
 
-#if defined(LINUX)
     zpool_errata_t errata;
     reason = zpool_get_status(zhp, &msgId, &errata);
-#else
-    reason = zpool_get_status(zhp, &msgId);
-#endif
 
     config = zpool_get_config(zhp, NULL);
 
@@ -103,6 +99,12 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "currently being resilvered.  The pool will\n\tcontinue "
                         "to function, possibly in a degraded state.\n"
                         "action: Wait for the resilver to complete.\n");
+                break;
+
+            case ZPOOL_STATUS_CORRUPT_CACHE:
+                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: The ZFS cache file is "
+                        "corrupted.\n"
+                        "action: Recover the pools by running 'zpool import'.\n");
                 break;
 
             case ZPOOL_STATUS_CORRUPT_DATA:
@@ -191,6 +193,7 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "to be recovered.\n");
                 break;
 
+            case ZPOOL_STATUS_IO_FAILURE_MMP:
             case ZPOOL_STATUS_IO_FAILURE_WAIT:
             case ZPOOL_STATUS_IO_FAILURE_CONTINUE:
                 (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: One or more devices are "
@@ -208,7 +211,18 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "device(s) and run 'zpool online',\n"
                         "\tor ignore the intent log records by running "
                         "'zpool clear'.\n");
+                break;
 
+            case ZPOOL_STATUS_BAD_GUID_SUM:
+                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: One or more top level "
+                        "devices are missing.\n"
+                        "\tWaiting for adminstrator intervention to fix the "
+                        "faulted pool.\n"
+                        "action: Attach the missing device(s).\n");
+                break;
+
+            case ZPOOL_STATUS_HOSTID_ACTIVE:
+            case ZPOOL_STATUS_HOSTID_REQUIRED:
             case ZPOOL_STATUS_HOSTID_MISMATCH:
                 (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: Mismatch between pool hostid "
                         "and system hostid on imported pool.\n\tThis pool was "
@@ -220,10 +234,21 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "\tThen import it to correct the mismatch.\n");
                 break;
 
+            case ZPOOL_STATUS_ERRATA:
+                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: The ZFS pool contains an on-disk "
+                        "format incompatibility.\n"
+                        "Affected pools must be imported and scrubbed using the current ZFS version.\n"
+                        "This will return the pool to a state in which it may be imported by other implementations.\n"
+                        "action: Scrub the affected pool with 'zpool scrub'.\n");
+                break;
+
+            case ZPOOL_STATUS_OK:
+                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: OK\n");
+                break;
+
 
             default:
-                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: OK\n");
-                assert(reason == ZPOOL_STATUS_OK);
+                (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: Unknown ZFS status detected\n");
         }
     }
 

commit 96f2c6617d19ec4747f82c86e666dbf1f0dfeb52

fix: build fs/zfs on FreeBSD prior to 13
d6dab2c broke fs/zfs on FreeBSD versions prior to 13. Some of the
ZFS-related symbols included in that commit are not in FreeBSD's ZFS
implementation (but were found in OpenZFS). FreeBSD did not adopt
OpenZFS until FreeBSD 13.

This commit enables building of fs/zfs on FreeBSD both with and without
OpenZFS.

modules/fs/zfs/src/Makefile
@@ -40,6 +40,10 @@ LDADD= 		-lzfs -lnvpair
 
 endif 
 
+ifeq  ($(and $(shell test $(UNAME_S) == "FreeBSD"; echo $$?),$(shell test `uname -K` -lt 1300000; echo $$?)),0)
+CFLAGS+=	-DFREEBSD_NO_OPENZFS
+endif
+
 all: zio.c $(OBJ)
 	$(CC) $(CFLAGS) $(DPADD) -o $(PROG) $(OBJ) $(LDADD)
 

modules/fs/zfs/src/vdev_status.c
@@ -23,8 +23,12 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
     nvlist_t * config, * nvroot;
     vdev_stat_t * vs;
 
+#if defined(FREEBSD_NO_OPENZFS)
+    reason = zpool_get_status(zhp, &msgId);
+#else
     zpool_errata_t errata;
     reason = zpool_get_status(zhp, &msgId, &errata);
+#endif
 
     config = zpool_get_config(zhp, NULL);
 
@@ -234,6 +238,7 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "\tThen import it to correct the mismatch.\n");
                 break;
 
+#if !defined(FREEBSD_NO_OPENZFS)
             case ZPOOL_STATUS_ERRATA:
                 (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: The ZFS pool contains an on-disk "
                         "format incompatibility.\n"
@@ -241,6 +246,7 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
                         "This will return the pool to a state in which it may be imported by other implementations.\n"
                         "action: Scrub the affected pool with 'zpool scrub'.\n");
                 break;
+#endif
 
             case ZPOOL_STATUS_OK:
                 (void) snprintf(zstat->err_message, ERR_MESSAGE_SIZE, "status: OK\n");